73cd7e58543552a3bf392126cd170b77f35c2311
Jerald Young <youngjerald239@gmail.com>
2024-03-15 18:37:26 -0500

    added sky glb and adjusted scale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import {Suspense} from 'react'
 import {Canvas} from '@react-three/fiber'
 import  Loader from '../components/Loader'
+import Sky from '../models/Sky'
 import Island from '../models/Island'
+
 
 {/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
@@ -19,7 +21,7 @@ const Home = () => {
       screenPosition = [0, -6.5, -43]
     } else 
     {
-      screenScale = [1, 1, 1]
+      screenScale = [0.3, 2, 2]
     }
     
     return [screenScale, screenPosition, rotation]
@@ -31,11 +33,11 @@ const Home = () => {
     <section className="w-full h-screen relative">
       <Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
-          <directionalLight  postion={[1, 1, 1]} intensity={20}/>
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight  postion={[1, 1, 1]} intensity={1}/>
+          <ambientLight intensity={-4}/>
+          <pointLight postion={[1, 1, 1]} intensity={5000}/>
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#b1e1ff" intensity={1}/>
+          <Sky />
           <Island 
             position={islandPosition}
             scale={islandScale}
